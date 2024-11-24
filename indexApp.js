d2ae5ebd650532fcb57ace917437fdd98614c218
Chance Smith <smit3974@msu.edu>
2024-11-24 00:45:30 -0500

    Format

---
+++ indexApp.js
@@ -0,0 +1,6 @@
+// Redirect to homepage when the logo is clicked
+document.getElementById("logo-button").addEventListener("click", handleClick);
+function handleClick() {
+    window.location.href = "/"; // Redirect to the root/homepage
+}
+
